66c17d8b16251c66d571d07e23bcadb894eff74e
mmorales4050 <mmorales4050@gmail.com>
2019-02-11 10:47:38 -0500

    Jukebox CLI

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,64 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:\n
+  - help : displays this help message\n
+  - list : displays a list of songs to play\n
+  - play : lets you choose a song to play\n
+  - exit : exits this program"
+end
+
+def list(songs)
+  output = ""
+  songs.each_with_index do |song, index|
+    output = output + "#{index + 1}. #{song}\n"
+  end
+  puts output
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+
+  song_choice = gets.chomp
+  song_numbers = (1..9).to_a
+  if song_numbers.include?(song_choice.to_i)
+    song_numbers.each do |number|
+      if song_choice == number.to_s
+        puts "Playing #{songs[number - 1]}"
+      end
+    end
+  elsif songs.include?(song_choice)
+    songs.each do |song|
+      if song_choice == song
+        puts "Playering #{song}"
+      end
+    end
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  # Show user command options
+  help
+
+  while true
+    puts "Please enter a command:"
+    response = gets.chomp
+    if response == "list"
+      list(songs)
+    elsif response == "play"
+      play(songs)
+    elsif response == "help"
+      help
+    elsif response == "exit"
+      exit_jukebox
+      break
+    end
+  end
+end
